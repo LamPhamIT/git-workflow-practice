5b5047d6ab345b41639c3354a8174c5a68e49214
Phạm Tấn Lam <phamtanlamBE@gmail.com>
2025-06-24 11:24:44 +0700

    resolve conflict by remove getHiInVietnamese

--- hello.js
+++ hello.js
@@ -1,12 +1,7 @@
 function getHello() {
   return "Hello, World!";
 }
-<<<<<<< HEAD
-function getHiInVietNamese() {
-  return "Xin chao"
-=======
 
 function getHi() {
   return "Hi, there!";
->>>>>>> b86af66 (add say hi function)
 }
