af941225753240d077f9e74cf3cc961640a832fa
lamphamit <phamtanlambe@gmail.com>
2025-06-24 11:18:38 +0700

    resolve conflict

--- hello.js
+++ hello.js
@@ -1,6 +1,12 @@
 function getHello() {
   return "Hello, World!";
 }
+<<<<<<< HEAD
 function getHiInVietNamese() {
   return "Xin chao"
+=======
+
+function getHi() {
+  return "Hi, there!";
+>>>>>>> b86af66 (add say hi function)
 }
